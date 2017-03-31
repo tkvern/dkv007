3c5c69cba2b76723df3e8438c658ad6ef09a2fed
liujun <lj90214@gmail.com>
2017-03-31 17:33:38 +0800

    Init project

---
+++ app/Models/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username', 'email', 'password', 'is_company', 'name', 'country', 'region'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function orders() {
+        return $this->hasMany(TaskOrder::class);
+    }
+
+    public function tasks() {
+        return $this->hasMany(Task::class);
+    }
+}
