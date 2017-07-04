d5516eddfda7c32d3bda50a710b3d05d69007c0b
tkvern <tkvern@qq.com>
2017-07-04 10:38:36 +0800

    修复bug

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -40,7 +40,7 @@
                     console.log(data.access_token);
                     access_token = data.access_token;
                     $("#fileuploader").uploadFile({
-                        url: "http://cloud.visiondk.com/api/upload/file",
+                        url: "/api/upload/file",
                         fileName: "myfile",
                         maxFileSize: 50*1024*1024,
                         multiple: false,
@@ -54,7 +54,7 @@
                         deleteCallback: function (data, pd) {
                             var data = JSON.parse(data);
                             for (var i = 0; i < data.length; i++) {
-                                $.post("http://cloud.visiondk.com/api/upload/delete", {op: "delete",name: data[i]});
+                                $.post("/api/upload/delete", {op: "delete",name: data[i]});
                             }
                             $("input[name='cover_image']").val('');
                             pd.statusbar.hide(); //You choice.
@@ -63,18 +63,21 @@
                         onSuccess:function(files,data,xhr,pd) {
                             // $("input[name='cover_image']").val(JSON.parse(data)[0])
                             console.log(data);
-                            debugger
-                            var data = JSON.parse(data)[0];
-                            $("#preview").empty();
-                            $("#preview").append("<p style='color:red; font-size: 24px;'>登录已失效，请刷新后重新登录!<p/><div id='qrcode'></div><br><a href='" + data.url + "' target='_blank'>" + data.url + "</a>")
-                            qrcode = new QRCode("qrcode", {
-                                text: data.url,
-                                width: 256,
-                                height: 256,
-                                colorDark : "#000000",
-                                colorLight : "#ffffff",
-                                correctLevel : QRCode.CorrectLevel.H
-                            });
+                            if (data.err_code == 0) {
+                                var data = JSON.parse(data)[0];
+                                $("#preview").empty();
+                                $("#preview").append("<p style='color:red; font-size: 24px;'>登录已失效，请刷新后重新登录!<p/><div id='qrcode'></div><br><a href='" + data.url + "' target='_blank'>" + data.url + "</a>")
+                                qrcode = new QRCode("qrcode", {
+                                    text: data.url,
+                                    width: 256,
+                                    height: 256,
+                                    colorDark : "#000000",
+                                    colorLight : "#ffffff",
+                                    correctLevel : QRCode.CorrectLevel.H
+                                });
+                            } else {
+                                alert(data.err_msg);
+                            }
                             
                         },
                     });
