74c6912a821848bfe14abe837784c8f7327a9b8d
tkvern <tkvern@qq.com>
2017-12-04 17:30:15 +0800

    update 404 blade

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>量子视觉云</title>
+</head>
+<body>
+    <h2>{{ $exception->getMessage() }}</h2>
+    <p>资源正在处理中...</p>
+    <p>请稍后访问</p>
+</body>
+</html>
